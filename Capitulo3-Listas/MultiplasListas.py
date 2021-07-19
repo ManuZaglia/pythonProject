1fef013b94350583cbcba122af683eb9b213c925
ManuZaglia <zaglia20@gmail.com>
2021-07-19 13:52:35 -0300

    Correção

--- Capitulo3-Listas/MultiplasListas.py
+++ Capitulo3-Listas/MultiplasListas.py
@@ -1,12 +1,12 @@
 equipamentos = []
 valores = []
-sereais = []
+seriais = []
 departamentos = []
 resposta = "S"
 while resposta == "S":
     equipamentos.append(input("Equipamentos: "))
     valores.append(float(input("Valor: ")))
-    sereais.append(int(input("Número Serial: ")))
+    seriais.append(int(input("Número Serial: ")))
     departamentos.append(input("Departamento: "))
     resposta = input("Digite S para continuar: ").upper()
 
@@ -14,5 +14,11 @@ for indice in range(0,len(equipamentos)):
     print("Equipamento..:", (indice+1))
     print("Nome........:", equipamentos[indice])
     print("Valor.......:", valores[indice])
-    print("Serial......:", sereais[indice])
+    print("Serial......:", seriais[indice])
     print("Departamento:", departamentos[indice])
+
+busca=input("Digite o nome do equipamento que deseja buscar: ")
+for indice in range(0,len(equipamentos)):
+  if busca==equipamentos[indice]:
+    print("Valor..: ", valores[indice])
+    print("Serial.:", seriais[indice])
